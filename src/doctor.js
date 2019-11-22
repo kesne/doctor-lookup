c224f5b0464b79ee34443ab38a72ae8a6716d52e
Jaime Gensler <jaimegensler0@gmail.com>
2019-11-22 11:55:31 -0800

    fix request location bug in fetch url

--- src/doctor.js
+++ src/doctor.js
@@ -3,7 +3,7 @@ export class Doctor {
         let doctors = [];
 
         try {
-            const response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${searchTerm}&practice_uid=45.5135%C2%B0%20N%2C%20122.6801%C2%B0%20W&location=45.514%2C-122.680%2C100&user_location=45.514%2C-122.680&sort=best-match-desc&skip=0&limit=10&user_key=${process.env.API_KEY}`);
+            const response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${searchTerm}&location=45.512%2C%20-122.658%2C50&user_location=45.512%2C%20-122.658&sort=best-match-desc&skip=0&limit=10&user_key=${process.env.API_KEY}`);
             if(response.ok) {
                 const jsonResponse = await response.json();
                 console.log(jsonResponse);
