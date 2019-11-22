5e2098a529bed7bf081c23db1e2971ab7ddc05e4
Jaime Gensler <jaimegensler0@gmail.com>
2019-11-22 14:53:52 -0800

    add cullSearchData function, restructure searchBetterDoctor function

--- src/search.js
+++ src/search.js
@@ -1,31 +1,39 @@
-export async function search(query){
-    let doctors = [];
+import { Doctor } from './doctor.js';
+
+export async function searchBetterDoctor(query) {
     try {
         const response = await fetch(`https://api.betterdoctor.com/2016-03-01/doctors?query=${query}&location=45.512%2C%20-122.658%2C50&user_location=45.512%2C%20-122.658&sort=best-match-desc&skip=0&limit=10&user_key=${process.env.API_KEY}`);
-        if(response.ok) {
+
+        if (response.ok) {
             const jsonResponse = await response.json();
-            console.log(jsonResponse);
-        }
-        else {
+            return cullSearchData(jsonResponse.data)
+        } else {
             throw new Error(response.status);
         }
     } catch (error) {
-        console.error(`API Error: ${error}`);
+        console.error(`BetterDoctor API Error: ${error}`);
     }
 }
 
-// jsonResponse.data[i].profile.first_name
-// jsonResponse.data[i].profile.last_name
-// jsonResponse.data[i].profile.title
-//`${jsonResponse.data[i].profile.first_name} ${jsonResponse.data[i].profile.last_name}, ${}
-// jsonResponse.data[i].profile.image_url
-//
-// jsonResponse.data[i].practices[0].name;
-// jsonResponse.data[i].practices[0].visit_address.street
-// jsonResponse.data[i].practices[0].visit_address.street2
-// jsonResponse.data[i].practices[0].visit_address.city
-// jsonResponse.data[i].practices[0].visit_address.state
-// jsonResponse.data[i].practices[0].visit_address.zip
-// jsonResponse.data[i].practices[0].within_search_area
-//
-// jsonResponse.data[i].practices[0].website;
+function cullSearchData(searchData) {
+    let doctors = [];
+    searchData.forEach((searchResult) => {
+        doctors.push(
+            new Doctor(
+                searchResult.profile.first_name,
+                searchResult.profile.last_name,
+                searchResult.profile.title,
+                searchResult.profile.image_url,
+                searchResult.practices[0].name,
+                searchResult.practices[0].visit_address.street,
+                searchResult.practices[0].visit_address.street2,
+                searchResult.practices[0].visit_address.city,
+                searchResult.practices[0].visit_address.state,
+                searchResult.practices[0].visit_address.zip,
+                searchResult.practices[0].within_search_area
+                searchResult.practices[0].website;
+            )
+        );
+    });
+    return doctors;
+}
